1152b4a7ef6e9a0261a4d64fce4580c1d6a93f31
omshreya <shreya.patil@globant.com>
2018-09-19 10:58:03 +0530

    Addition is done

---
+++ src/sample/Addition.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Addition 
+{
+	public static void main(String args[])
+	{
+		int a=10,b=20;
+		int c;
+		c=a+b;
+		System.out.println("Addition of two numbers is:"+ a+b + c);
+	}
+	
+}
